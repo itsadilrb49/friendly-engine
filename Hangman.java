6a2852e2b6a58ce632d54197e924ffdba21a35c0
itsadilrb49 <106427212+itsadilrb49@users.noreply.github.com>
2022-06-17 16:13:36 +0530

    Add files via upload

---
+++ Hangman.java
@@ -0,0 +1,11 @@
+//package game;
+
+public class Hangman
+{
+	public static void main(String[] args)
+	{
+		Menu obj1 = new Menu();
+	
+		obj1.addComponent();
+	}
+}
---
+++ Menu.java
@@ -0,0 +1,81 @@
+//ch-8 Act 1/3
+
+//package game;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Menu extends JFrame implements ActionListener
+{
+	JButton option1;
+	JButton option2;
+	JButton option3;
+	JLabel name;
+	
+	public Menu()
+	{
+		option1 = new JButton("Play Game");
+		option2 = new JButton("View Instruction");
+		option3 = new JButton("Exit");
+		name = new JLabel("HANGMAN");
+		name.setFont(new Font("Serif", Font.PLAIN, 24));
+		setTitle("Hangman Game");
+		setSize(300,200);
+		setResizable(false);
+		setVisible(true);
+		
+		option1.addActionListener(this);
+		option2.addActionListener(this);
+		option3.addActionListener(this);
+	}
+	
+	public void addComponent()
+	{
+		setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weighty = 0.1;
+		c.anchor = c.CENTER;
+		add(name,c);
+		
+		c.gridx = 0;
+		c.gridy = 1;
+		c.fill = c.HORIZONTAL;
+		add(option1, c);
+		
+		c.gridx = 0;
+		c.gridy = 2;
+		c.fill = c.HORIZONTAL;
+		add(option2, c);
+		
+		c.gridx = 0;
+		c.gridy = 3;
+		c.fill = c.HORIZONTAL;
+		add(option3, c);
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource() == option1)
+		{
+			GameWindow obj = new GameWindow();
+			obj.startGame();
+		}
+		else if(e.getSource() == option2)
+		{
+			JOptionPane.showMessageDialog(this, "1. You can guess the word by clicking the character from the virtual keypad. \n2. You can select a maximum of 5 incorrect letters. \n3. The incorrect guess will be highlighted with red color, and the correct one will be highlighted with green color.", "Instructions", JOptionPane.INFORMATION_MESSAGE);
+		}
+		else if(e.getSource() == option3)
+		{
+			System.exit(0);
+		}
+	}
+}
